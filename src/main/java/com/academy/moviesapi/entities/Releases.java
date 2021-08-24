5c2e39cf3d3d7739779eaa1396160aa859724b76
Personal.Carolina <scchavezd@gmail.com>
2021-08-24 14:29:26 -0500

    adding getters and setters

--- src/main/java/com/academy/moviesapi/entities/Releases.java
+++ src/main/java/com/academy/moviesapi/entities/Releases.java
@@ -11,6 +11,47 @@ public class Releases {
     private Long id;
     private String title;
     private String poster;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     private Double rating;
     private String description;
 }
